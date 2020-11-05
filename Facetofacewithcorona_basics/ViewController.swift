a59f4515fb57a65c0457c724df14a0990a23f6fc
Tamara N <tnatassja@gmail.com>
2020-11-05 20:04:07 +1100

    Initial Commit

---
+++ Facetofacewithcorona_basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Facetofacewithcorona_basics
+//
+//  Created by Tamara Natassja on 5/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
